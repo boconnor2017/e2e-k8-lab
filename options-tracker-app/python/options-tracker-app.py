0e3873fcf1bc05c6f663eded4329662ef40cc951
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-07 16:44:28 -0500

    Create options-tracker-app.py

---
+++ options-tracker-app/python/options-tracker-app.py
@@ -0,0 +1,14 @@
+# This is an open source project from the E2E lab.
+# Code and documentation can be found at https://github.com/boconnor2017/e2e-k8-lab/tree/main/options-tracker-app
+#
+# Options Tracker App Version 1.0
+# January 2022
+# Author: Brendan O'Connor
+
+print("*****************************************************")
+print("*****************************************************")
+print(" ")
+print("Welcome to the Options Tracker App -- Version 1.0")
+print(" ")
+print("*****************************************************")
+print("*****************************************************")
