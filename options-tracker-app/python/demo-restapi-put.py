9a6168295964db0c58b743c858701dba6b7beac2
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-08 09:58:37 -0500

    Delete demo-restapi-put.py

--- options-tracker-app/python/demo-restapi-put.py
+++
@@ -1,9 +0,0 @@
-import requests
-api_url = "https://jsonplaceholder.typicode.com/todos/10"
-response = requests.get(api_url)
-print("Initial GET:")
-print(response.json())
-todo = {"userId": 1, "title": "Wash car", "completed": True}
-response = requests.put(api_url, json=todo)
-print("Updated request via PUT:")
-print(response.json())
