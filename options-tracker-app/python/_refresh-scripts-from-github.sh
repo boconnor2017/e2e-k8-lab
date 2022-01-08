3240b6a3d46689a185564931574ee88baa2ce653
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-08 09:58:07 -0500

    Update _refresh-scripts-from-github.sh

--- options-tracker-app/python/_refresh-scripts-from-github.sh
+++ options-tracker-app/python/_refresh-scripts-from-github.sh
@@ -1,8 +1,5 @@
 rm *
 curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/options-tracker-app.py >> options-tracker-app.py
 curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/config.py >> config.py
-curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/demo-restapi-get.py >> demo-restapi-get.py
-curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/demo-restapi-put.py >> demo-restapi-put.py
-curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/demo-restapi-post.py >> demo-restapi-post.py
 curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/demo-restapi.py >> demo-restapi.py
 curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/_refresh-scripts-from-github.sh >> _refresh-scripts-from-github.sh
