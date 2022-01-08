f50468316b6bbda5fd8f6b0c85391ab87a8b770c
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-08 09:07:30 -0500

    Update _refresh-scripts-from-github.sh

--- options-tracker-app/python/_refresh-scripts-from-github.sh
+++ options-tracker-app/python/_refresh-scripts-from-github.sh
@@ -1,3 +1,4 @@
+rm *
 curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/options-tracker-app.py >> options-tracker-app.py
 curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/config.py >> config.py
 curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/demo-restapi-get.py >> demo-restapi-get.py
