7638116474b6247368c43a569f24f4cadc4522f9
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-08 09:58:29 -0500

    Delete demo-restapi-post.py

--- options-tracker-app/python/demo-restapi-post.py
+++
@@ -1,8 +0,0 @@
-import requests
-api_url = "https://jsonplaceholder.typicode.com/todos"
-todo = {"userId": 1, "title": "Buy milk", "completed": False}
-response = requests.post(api_url, json=todo)
-print("POST:")
-print(response.status_code)
-print(response.headers["Content-Type"])
-print(response.json())
