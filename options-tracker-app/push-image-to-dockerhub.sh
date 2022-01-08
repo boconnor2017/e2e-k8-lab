5f9c0f1126c44bf9cf077502626b1a608e12b1f2
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-08 09:27:13 -0500

    Update push-image-to-dockerhub.sh

--- options-tracker-app/push-image-to-dockerhub.sh
+++ options-tracker-app/push-image-to-dockerhub.sh
@@ -1,3 +1,3 @@
 # PREREQ: Login to dockerhub using docker login -u vmwe2e -p <password>
-docker tag boc-base-python-image-001 vmwe2e/boc-base-python-image-001
-docker push vmwe2e/boc-base-python-image-001
+docker tag options-tracker-app-001  vmwe2e/options-tracker-app-001
+docker push vmwe2e/options-tracker-app-001 
