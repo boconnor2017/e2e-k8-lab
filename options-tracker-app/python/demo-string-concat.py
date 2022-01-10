09a7b3a3e413607c29b2c41d2152bfe7feaf8d66
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-10 14:01:25 -0500

    Create demo-string-concat.py

---
+++ options-tracker-app/python/demo-string-concat.py
@@ -0,0 +1,15 @@
+a = ('''\
+            <html>
+            <head>
+                <title>Options Tracker App Version 1.0</title>
+            </head>
+            <body>
+                <h1>Options Trader Application</h1>
+       	''')
+b = "SOMETHING COMPLETELY RANDOM!!!!"
+c = ('''\
+            </body>
+            </html>
+        ''')
+d = a+b+c
+print(d)
