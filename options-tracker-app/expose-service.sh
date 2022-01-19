160270d6227a2ebfb080b120586f66bafa73dbf0
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-19 09:59:31 -0500

    Update and rename expose-service-minikube.sh to expose-service.sh

--- options-tracker-app/expose-service.sh
+++ options-tracker-app/expose-service.sh
@@ -1,3 +1,2 @@
-# MINIKUBE ONLY
+# MINIKUBE ONLY: open second SSH session, and run minikube tunnel
 kubectl expose deployment options-tracker-app --type=LoadBalancer --port=80
-minikube service options-tracker-app
