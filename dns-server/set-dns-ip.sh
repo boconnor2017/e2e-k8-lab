bfe55ff78f54de732c55ed035d4cf197d9451752
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-24 08:37:44 -0500

    Update set-dns-ip.sh

--- dns-server/set-dns-ip.sh
+++ dns-server/set-dns-ip.sh
@@ -1,3 +1,7 @@
 #Run on photon
+echo Changing IP Address..
+echo You are about to lose your SSH Session!
+echo Start a new session at 172.16.0.9
+echo
 ifconfig eth0 172.16.0.9 netmask 255.255.255.0
 route add default gateway 172.16.0.1
