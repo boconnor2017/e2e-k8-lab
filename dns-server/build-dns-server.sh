6b50432da792d31b76966758bf1f3c1e740c1e34
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-23 20:55:38 -0500

    Update build-dns-server.sh

--- dns-server/build-dns-server.sh
+++ dns-server/build-dns-server.sh
@@ -1,3 +1,4 @@
 yum -y install unbound
 cp /etc/unbound/unbound.conf /etc/unbound/unbound.conf_BACKUP
 rm -f /etc/unbound/unbound.conf
+curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/dns-server/unbound.conf >> /etc/unbound/unbound.conf
