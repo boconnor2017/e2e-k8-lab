f84a2148c2734895381a002857a9831cd2a47e42
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-23 20:58:29 -0500

    Update build-dns-server.sh

--- dns-server/build-dns-server.sh
+++ dns-server/build-dns-server.sh
@@ -2,3 +2,10 @@ yum -y install unbound
 cp /etc/unbound/unbound.conf /etc/unbound/unbound.conf_BACKUP
 rm -f /etc/unbound/unbound.conf
 curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/dns-server/unbound.conf >> /etc/unbound/unbound.conf
+cat /etc/unbound/unbound.conf
+iptables -A INPUT -i eth0 -p udp --dport 53 -j ACCEPT
+iptables-save >/etc/systemd/scripts/ip4save
+iptables -L
+systemctl enable unbound
+systemctl start unbound
+systemctl status unbound
