caad79b78bca9caa075e5dd7066254f6f3ce6469
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-26 21:48:47 -0500

    Update build-dns-server.sh

--- dns-server/build-dns-server.sh
+++ dns-server/build-dns-server.sh
@@ -8,4 +8,4 @@ cp resolv.conf /etc/resolv.conf
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-docker-compose up
+docker-compose up &
