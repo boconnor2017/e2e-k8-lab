d0b80723d5e4021389efb1eac6fddda9022ceae0
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2022-01-27 20:58:02 -0500

    Create setup-python-sdk.sh

---
+++ vsphere-api/setup-python-sdk.sh
@@ -0,0 +1,5 @@
+curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/options-tracker-app/python/install-python3.sh >> install-python3.sh
+sh install-python3.sh
+yum -y install git
+python3 -m pip install pyvmomi
+python3 -m pip install git+https://github.com/vmware/pyvmomi.git
